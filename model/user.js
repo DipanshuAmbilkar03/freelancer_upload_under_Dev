dd6660bd81d3b606e306e1bbd237aadac16cce0f
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-12-07 17:55:40 +0530

    avater

--- model/user.js
+++ model/user.js
@@ -4,7 +4,12 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' }
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  avatar: { 
+    type: String, 
+    default: 'https://cdn-icons-png.flaticon.com/512/149/149071.png', 
+    trim: true 
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
