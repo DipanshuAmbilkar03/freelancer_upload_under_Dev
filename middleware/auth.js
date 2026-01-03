d4544e79a9b3026a8cbcb83b6afa590ee746a8bd
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-01-04 00:19:57 +0530

    login auth (_id is not getting added in DB)

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,54 @@
-// Simple session-based authentication middleware
+// // Simple session-based authentication middleware
 
-function ensureAuthenticated(req, res, next) {
-  if (!req.session.user) {
+// function ensureAuthenticated(req, res, next) {
+//   if (!req.session.user) {
+//     return res.redirect('/login');
+//   }
+//   req.user = req.session.user;
+//   next();
+// }
+
+
+// function isLoggedIn(req, res, next) {
+//   if (req.session.user) {
+//     return next();
+//   } 
+
+//   // Redirect to login with a query parameter indicating the message
+//   return res.redirect('/login?message=Please+login+to+access+this+page');
+// }
+
+// module.exports = { ensureAuthenticated ,isLoggedIn};   
+
+
+const User = require('../model/user');
+
+async function ensureAuthenticated(req, res, next) {
+  if (!req.session || !req.session.user) {
     return res.redirect('/login');
   }
-  req.user = req.session.user;
-  next();
+
+  try {
+    const user = await User.findById(req.session.user._id);
+
+    if (!user) {
+      req.session.destroy();
+      return res.redirect('/login');
+    }
+
+    req.user = user; 
+    next();
+  } catch (err) {
+    console.error("Auth error:", err);
+    return res.redirect('/login');
+  }
 }
 
-
 function isLoggedIn(req, res, next) {
-  if (req.session.user) {
+  if (req.session && req.session.user) {
     return next();
-  } 
-
-  // Redirect to login with a query parameter indicating the message
+  }
   return res.redirect('/login?message=Please+login+to+access+this+page');
 }
 
-module.exports = { ensureAuthenticated ,isLoggedIn};   
-
-
+module.exports = { ensureAuthenticated, isLoggedIn };
