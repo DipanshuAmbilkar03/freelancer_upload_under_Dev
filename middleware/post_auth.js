f844d0e8a83b9cc7d7e18e361c6c02b685a45c7f
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-01-12 22:38:45 +0530

    login auth

--- middleware/post_auth.js
+++ middleware/post_auth.js
@@ -1,21 +1,43 @@
-const jwt = require('jsonwebtoken');
+// function ensureAuthenticated(req, res, next) {
+//     if (!req.session || !req.session.user) {
+//     return res.status(401).redirect('/login');
+//     }
+
+//     try {
+//     // Attach user info for downstream routes
+//     req.user = {
+//         _id: req.session.user._id,
+//         username: req.session.user.username,
+//         avatar: req.session.user.avatar
+//         };
+    
+//         next();
+//     }catch (err) {
+//         console.error('Auth middleware error:', err);
+//         return res.status(401).redirect('/login');
+//     }
+
+// }
+
+// module.exports = ensureAuthenticated;
+
 
 function ensureAuthenticated(req, res, next) {
     if (!req.session || !req.session.user) {
-    return res.status(401).redirect('/login');
+        return res.redirect('/login');
     }
 
     try {
-    // Attach user info for downstream routes
-    req.user = {
-        userId: req.session.user.id,
+        req.user = {
+        _id: req.session.user._id,
         username: req.session.user.username,
         avatar: req.session.user.avatar
         };
-    
+
         next();
-    }catch {
-        req.status(401).json({error : "Invalid Token"})
+    } catch (err) {
+        console.error('Auth middleware error:', err);
+        return res.redirect('/login');
     }
 }
 
