8052ddd16ba79b72d4d4cafa2a1ddd09becf48a9
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-01-12 22:39:58 +0530

    post route

--- routes/web.js
+++ routes/web.js
@@ -4,7 +4,7 @@ const Assignment = require('../model/assignment');
 const { ensureAuthenticated, isLoggedIn } = require('../middleware/auth');
 const mongoose = require('mongoose');
 const assignmentpdfs = require('../model/assignmentPdf');
-const user = require("../model/user");
+const User = require("../model/user.js");
 
 // Home page route
 router.get('/', (req, res) => {
@@ -21,21 +21,16 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
-// router.get("/test/img",(req,res) => {
-//     console.log(user.body);
-//     res.render("test.ejs",{user});
-// })
-
 // Dashboard route
-router.get('/dashboard', ensureAuthenticated,isLoggedIn, async (req, res) => {
+router.get('/dashboard', ensureAuthenticated, async (req, res) => {
     try {
         const assignments = await Assignment.find({ postedBy: req.user.userId })
-        .populate({
-            path: 'pdfs',           
-            select: 'file uploadedBy', 
-        })
-        .sort({ createdAt: -1 });
-
+            .populate({
+                path: 'pdfs',
+                select: 'file uploadedBy',
+            })
+            .sort({ createdAt: -1 });
+            
         // const pdfs = await assignmentpdfs.find({assignment: userId });
 
         res.render('dashboard', { user: req.user, assignments });
@@ -48,8 +43,25 @@ router.get('/dashboard', ensureAuthenticated,isLoggedIn, async (req, res) => {
 // Post assignment route
 router.get('/post-assignment', ensureAuthenticated, (req, res) => {
     const user = req.user;
-    res.render('post-assignment',{user});
+    console.log(user);
+    
+    res.render('post-assignment', { user });
 });
+
+router.get(
+    '/savedAssignmemt',
+    ensureAuthenticated,
+    async (req, res) => {
+      const user = await User.findById(req.user._id)
+        .populate('savedAssignments');
+  
+      res.render('saved', {
+        assignments: user.savedAssignments || [],
+        user
+      });
+    }
+  );
+  
 
 // View all assignments route
 router.get('/assignments', async (req, res) => {
@@ -58,10 +70,10 @@ router.get('/assignments', async (req, res) => {
             .sort({ createdAt: -1 })
             .populate({
                 path: 'postedBy',
-                select: 'username avatar' 
+                select: 'username avatar'
             });
 
-        res.render('assignments', { assignments });
+        res.render('assignments', { assignments ,user: req.user});
     } catch (err) {
         console.error(err);
         res.status(500).send('Server error');
@@ -72,7 +84,7 @@ router.get('/assignments', async (req, res) => {
 router.get('/assignments/:id', async (req, res) => {
     const { id } = req.params;
     console.log('Assignment ID received:', req.params.id, 'Full URL:', req.originalUrl);
-    
+
     if (!mongoose.Types.ObjectId.isValid(id)) {
         console.error('Invalid assignment ID:', id);
         return res.status(400).send('Invalid assignment ID');
@@ -80,11 +92,11 @@ router.get('/assignments/:id', async (req, res) => {
 
     try {
         const assignment = await Assignment.findById(id)
-        .populate('postedBy', 'username email')
-        .populate({
-        path: 'pdfs',
-        select: 'file uploadedBy',
-    });
+            .populate('postedBy', 'username email')
+            .populate({
+                path: 'pdfs',
+                select: 'file uploadedBy',
+            });
         if (!assignment) return res.status(404).send('Assignment not found');
         res.render('assignment-detail', { assignment, user: req.user });
     } catch (err) {
@@ -93,33 +105,21 @@ router.get('/assignments/:id', async (req, res) => {
     }
 });
 
-router.get('/file/:id', (req, res) => {
-    const gfs = req.app.locals.gfs;
-    gfs.files.findOne({ _id: mongoose.Types.ObjectId(req.params.id) }, (err, file) => {
-        if (!file || file.length === 0) return res.status(404).send('No file found');
-
-        const readstream = gfs.createReadStream(file.filename);
-        res.set('Content-Type', file.contentType);
-        readstream.pipe(res);
-    });
-});
-
-
 router.get('/logout', (req, res) => {
     req.session.destroy(err => {
         if (err) {
             console.error('Session destruction error:', err);
             return res.redirect('/');
         }
-    
+
         res.clearCookie('connect.sid', { path: '/' });
         res.clearCookie('freelancer.sid', { path: '/' });
         res.clearCookie('username-localhost-8888', { path: '/' });
-    
+
         req.session = null;
-    
+
         return res.redirect('/login');
-        });
     });
+});
 
 module.exports = router;
